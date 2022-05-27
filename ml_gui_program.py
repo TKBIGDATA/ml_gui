45e3d407b54fe203f22d818405ad9e72a79cce79
TKBIGDATA <t.bigdata.3@gmail.com>
2022-05-27 09:12:14 +0900

    QTableView

--- ml_gui_program.py
+++ ml_gui_program.py
@@ -15,8 +15,15 @@ class UI(QMainWindow):
         self.Browse = self.findChild(QPushButton , "Browse")
         self.columns = self.findChild(QListWidget , "column_list")
         self.table = self.findChild(QTableView , "tableView")
+        self.data_shape = self.findChild(QLabel , "shape")
+        self.label_2 = self.findChild(QLabel , "label_2")
+        self.submit_btn = self.findChild(QPushButton , "Submit")
+        self.target_col = self.findChild(QLabel , "target_col")
+
 
         self.Browse.clicked.connect(self.getCSV)
+        self.columns.clicked.connect(self.target)
+        self.submit_btn.clicked.connect(self.set_target)
 
     def filldetails(self ,flag = 1):
         if flag ==0:
@@ -28,10 +35,12 @@ class UI(QMainWindow):
 
         for i , j in enumerate(self.column_list):
             # print (i,j)
-            stri = f'{j}------{str(self.df[j].dtype)}'
+            stri = f'{j} ------ {str(self.df[j].dtype)}'
             # print(stri)
             self.columns.insertItem(i, stri)
-        
+
+        x , y  = data.get_shape(self.df)
+        self.data_shape.setText(f'({x} ,{y})')
         self.fill_combo_box()
     
     def fill_combo_box(self):
@@ -47,8 +56,15 @@ class UI(QMainWindow):
         # print(self.filePath)
         if self.filePath !="":
             self.filldetails(0)
-            
-     
+
+    def target(self):
+        self.item = self.columns.currentItem()
+
+    def set_target(self):
+        self.target_value = str(self.item.text()).split()[0]
+        print(self.target_value)
+        self.target_col.setText(self.target_value)
+
         
 
         
