c6622c79c4c9e2a7fe0e642bfd7386cf0da7d5c3
TKBIGDATA <t.bigdata.3@gmail.com>
2022-05-31 13:05:48 +0900

    pre-trained

--- pre_trained.py
+++ pre_trained.py
@@ -18,7 +18,7 @@ import seaborn as sns
 class UI(QMainWindow):
     def __init__(self,df,target,pickle_model,path):
         super(UI, self).__init__()
-        uic.loadUi("../ui_files/pre_trained.ui", self)
+        uic.loadUi("./ui_files/pre_trained.ui", self)
         self.path=path
         global data 
         data=data_visualise.data_()
